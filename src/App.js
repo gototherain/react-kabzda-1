191b316fa8d24c636953bb786d1bdc652eacaf1f
gototherain <go.to.the.rain@gmail.com>
2022-11-16 13:16:11 +0600

    02

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
+    <div>
+      <div>First div</div>
+      <div className="App">
 
-      <ul>
-        <li>html</li>
-        <li>css</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
+        <ul>
+          <li>html</li>
+          <li>css</li>
+          <li>js</li>
+          <li>react</li>
+        </ul>
 
+      </div>
     </div>
   );
 }
