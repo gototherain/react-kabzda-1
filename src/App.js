9a37eff01632df2be629840fbf03553a1625c146
gototherain <go.to.the.rain@gmail.com>
2022-11-16 15:44:42 +0600

    02

--- src/App.js
+++ src/App.js
@@ -5,19 +5,38 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <div>First div</div>
-      <div className="App">
-
-        <ul>
-          <li>html</li>
-          <li>css</li>
-          <li>js</li>
-          <li>react</li>
-        </ul>
-
-      </div>
+      <Header />
+      <Technologies />
     </div>
   );
 }
+
+const Header = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contacts</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+const Technologies = () => {
+  return (
+    <div>
+
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+
+    </div>
+  );
+}
+
 
 export default App;
