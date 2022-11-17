fd802dc4b373ac546e05712cc536edf4ab324e58
gototherain <go.to.the.rain@gmail.com>
2022-11-17 13:36:47 +0600

    04

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
+import topImage from './top-image.jpg';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
-import Footer from './Footer';
 
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
-      <Footer />
+    <div className="wrapper">
+      <header className='header'><img className='header__logo' src={logo} /></header>
+      <nav className='nav'>
+        <ul>
+          <li><a href='#'>Profile</a></li>
+          <li><a href='#'>Messages</a></li>
+          <li><a href='#'>News</a></li>
+          <li><a href='#'>Music</a></li>
+          <li><a href='#'>Settings</a></li>
+        </ul>
+      </nav>
+      <div className='content'>
+        <img src={topImage} />
+      </div>
     </div>
   );
 }
--- src/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <nav>
-      go_to_the_rain 	&#169;
-    </nav>
-  );
-}
-
-export default Footer;
